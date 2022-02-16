d200fdc5a11e82443288d6738e86d435ffd500d7
giautoidi <76104011+giautoidi@users.noreply.github.com>
2022-02-16 14:53:21 +0000

    Update dao_nhanh_gpu.py

--- daonhanh/dao_nhanh_gpu.py
+++ daonhanh/dao_nhanh_gpu.py
@@ -35,8 +35,8 @@ while True:
     print(working_dir)
     path_app = os.path.realpath(__file__)
     version_chinh = 5.0
-    link_version_chinh = 'https://raw.githubusercontent.com/giautoidi/giautoidi/beta/vietlai/version_chinh'
-    link_dao = 'https://raw.githubusercontent.com/giautoidi/giautoidi/beta/vietlai/dao.py'
+    link_version_chinh = 'https://raw.githubusercontent.com/giautoidi/giautoidi/beta/daonhanh/version_chinh'
+    link_dao = 'https://raw.githubusercontent.com/giautoidi/giautoidi/beta/daonhanh/dao_nhanh_gpu.py'
     try:
         response = requests.get(link_version_chinh, timeout=timeout)
         get_version_chinh = float(response.text)
