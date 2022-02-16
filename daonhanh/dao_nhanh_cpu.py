4c03ba56961da252a1f21dd5a2b1e967303154ff
giautoidi <76104011+giautoidi@users.noreply.github.com>
2022-02-16 14:16:08 +0000

    Update dao_nhanh_cpu.py

--- daonhanh/dao_nhanh_cpu.py
+++ daonhanh/dao_nhanh_cpu.py
@@ -1,13 +1,12 @@
-try:
-    import os
-    import sys
-    import time
-    from sys import platform
-    import requests
-    import subprocess
-    #import multiprocessing
-except:
-    pass
+
+import os
+import sys
+import time
+from sys import platform
+import requests
+import subprocess
+#import multiprocessing
+
 
 if platform == "linux" or platform == "linux2":
     operate_system = 'lin'
